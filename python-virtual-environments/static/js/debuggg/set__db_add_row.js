258aa648de772e995531c1e6094e30da66a663e6
Pakkanut Mee-Im <55268141+aczostudio@users.noreply.github.com>
2020-01-29 17:22:53 +0700

    update vat net total

--- python-virtual-environments/static/js/debuggg/set__db_add_row.js
+++ python-virtual-environments/static/js/debuggg/set__db_add_row.js
@@ -24,6 +24,53 @@ $(document).ready(function()
     });
 });
 
+function cal_totalprice(p,a,v)
+{
+    var total_vat = 0.0;
+    var ctotal = 0.0 + (parseFloat(p.replace(/,/g,'')) * parseFloat(a.replace(/,/g,'')));
+    var cVat = 1.0 + parseFloat(v);
+    total_vat = ctotal*cVat;
+    
+    return total_vat;
+}
+
+function upd_netvat()
+{
+    var post_net = 0.0;
+    var post_vat = 0.0;
+
+    var table_r = document.getElementById("tab_logic");
+    // console.log(table_r.rows.length);
+    //console.log(table_r.rows[1].cells);
+    for (var i = 1; i < table_r.rows.length; ++i)
+    {
+        var f_pric = 'price'+(i-1);
+        var fd_price = document.getElementsByName(f_pric)[0].value;
+
+        var f_amoun = 'amount'+(i-1);
+        var fd_amoun = document.getElementsByName(f_amoun)[0].value;
+
+        cal_net = (parseFloat(fd_price.replace(/,/g,''))) * (parseFloat(fd_amoun.replace(/,/g,'')));
+        post_net = post_net + cal_net;
+
+        var f_vat = 'vat'+(i-1);
+        var fd_vat = document.getElementsByName(f_vat)[0].value;
+
+        cal_vat = cal_net * parseFloat(fd_vat);
+        post_vat = post_vat + cal_vat;
+    }
+    // console.log("UPDATE NET VAT TOTAL");
+
+    // document.getElementsByName("nettotal0")[0].disabled = true;
+    document.getElementsByName("pricetotal0")[0].value = (post_net).toLocaleString('en-US', {minimumFractionDigits: 2});
+
+    // document.getElementsByName("vattotal0")[0].disabled = true;
+    document.getElementsByName("vattotal0")[0].value = (post_vat).toLocaleString('en-US', {minimumFractionDigits: 2});
+
+    document.getElementsByName("nettotal0")[0].value = (post_net + post_vat).toLocaleString('en-US', {minimumFractionDigits: 2});
+
+}
+
 function update_db_row (product_data, table_index)
 {
     var str_table   = 'addr'+table_index;
@@ -35,7 +82,7 @@ function update_db_row (product_data, table_index)
     var str_total   = 'totalprice'+table_index;
     //console.log("STR PRODUCT : " + str_code);
     var table       = document.getElementById(str_table);
-    var chile_td    = table.getElementsByTagName("td");
+    //var chile_td    = table.getElementsByTagName("td");
     var code        = document.getElementsByName(str_code)[0];
     //var desc  = chile_td[1];
     var desc        = document.getElementsByName(str_desc)[0];
@@ -45,6 +92,7 @@ function update_db_row (product_data, table_index)
     var vat         = document.getElementsByName(str_vat)[0];
     //var total_p = chile_td[5];
     var total_p     = document.getElementsByName(str_total)[0];
+    total_p.disabled = true;
     
 
     //add_product_option(product_data,code);
@@ -80,11 +128,14 @@ function update_db_row (product_data, table_index)
     if(!desc.value || desc.value == product_data[0][1]){desc.value = product_data[0][1];}
     //desc.disabled = true;
     //console.log("PRIC VAL : " + pric.value );
-    if(!pric.value || pric.value == product_data[0][2]){pric.value = product_data[0][2];}
+    // if(!pric.value || pric.value == product_data[0][2]){pric.value = product_data[0][2];}
+    if(!pric.value){pric.value = product_data[0][2];}
     //pric.disabled = true;
     //code.selectedIndex = "0";
     if(!amoun.value){amoun.value = 0;}
     if(vat.selectedIndex !== 1){vat.selectedIndex = "0";}
+
+    pric.value = (parseFloat((pric.value).replace(/,/g,''))).toLocaleString('en-US', {minimumFractionDigits: 2});
 
     code.onchange = function()
     {
@@ -92,17 +143,20 @@ function update_db_row (product_data, table_index)
         var options = code.selectedIndex;
         // console.log(options);
         desc.value = product_data[options][1];
-        pric.value = product_data[options][2];
+        pric.value = (parseFloat(product_data[options][2])).toLocaleString('en-US', {minimumFractionDigits: 2});
         if(!!(amoun) && !!(pric))
         {
-            var ctotal = 0.0 + (+(pric.value) * +(amoun.value));
-            // console.log("VALUES PRIC : " + pric.value);
-            // console.log("VALUES AMOUN : " + amoun.value);
-            var cVat = 1.0 + +(vat.value);
-            // console.log("VALUES VAT : " + vat.value);
-            var total_vat = (ctotal*cVat).toFixed(2);
-            // console.log("TOTAL VAT : " + total_vat);
-            total_p.value = total_vat;
+            // var ctotal = 0.0 + (+(pric.value) * +(amoun.value));
+            // // console.log("VALUES PRIC : " + pric.value);
+            // // console.log("VALUES AMOUN : " + amoun.value);
+            // var cVat = 1.0 + +(vat.value);
+            // // console.log("VALUES VAT : " + vat.value);
+            // var total_vat = (ctotal*cVat).toFixed(2);
+            // // console.log("TOTAL VAT : " + total_vat);
+            // total_p.value = total_vat;
+            var cal_price = cal_totalprice(pric.value,amoun.value,vat.value);
+            total_p.value = (cal_price).toLocaleString('en-US', {minimumFractionDigits: 2});
+            upd_netvat();
         }
         else
         {
@@ -112,26 +166,32 @@ function update_db_row (product_data, table_index)
 
     amoun.onchange = function()
     {
-        var ctotal = 0.0 + (+(pric.value) * +(amoun.value));
-            // console.log("VALUES PRIC : " + pric.value);
-            // console.log("VALUES AMOUN : " + amoun.value);
-            var cVat = 1.0 + +(vat.value);
-            // console.log("VALUES VAT : " + vat.value);
-            var total_vat = (ctotal*cVat).toFixed(2);
-            // console.log("TOTAL VAT : " + total_vat);
-            total_p.value = total_vat;
+        // var ctotal = 0.0 + (+(pric.value) * +(amoun.value));
+        //     // console.log("VALUES PRIC : " + pric.value);
+        //     // console.log("VALUES AMOUN : " + amoun.value);
+        //     var cVat = 1.0 + +(vat.value);
+        //     // console.log("VALUES VAT : " + vat.value);
+        //     var total_vat = (ctotal*cVat).toFixed(2);
+        //     // console.log("TOTAL VAT : " + total_vat);
+        //     total_p.value = total_vat;
+        var cal_price = cal_totalprice(pric.value,amoun.value,vat.value);
+        total_p.value = (cal_price).toLocaleString('en-US', {minimumFractionDigits: 2});
+        upd_netvat();;
     }
 
     vat.onchange = function()
     {
-        var ctotal = 0.0 + (+(pric.value) * +(amoun.value));
-            // console.log("VALUES PRIC : " + pric.value);
-            // console.log("VALUES AMOUN : " + amoun.value);
-            var cVat = 1.0 + +(vat.value);
-            // console.log("VALUES VAT : " + vat.value);
-            var total_vat = (ctotal*cVat).toFixed(2);
-            // console.log("TOTAL VAT : " + total_vat);
-            total_p.value = total_vat;
+        // var ctotal = 0.0 + (+(pric.value) * +(amoun.value));
+        //     // console.log("VALUES PRIC : " + pric.value);
+        //     // console.log("VALUES AMOUN : " + amoun.value);
+        //     var cVat = 1.0 + +(vat.value);
+        //     // console.log("VALUES VAT : " + vat.value);
+        //     var total_vat = (ctotal*cVat).toFixed(2);
+        //     // console.log("TOTAL VAT : " + total_vat);
+        //     total_p.value = total_vat;
+        var cal_price = cal_totalprice(pric.value,amoun.value,vat.value);
+        total_p.value = (cal_price).toLocaleString('en-US', {minimumFractionDigits: 2});
+        upd_netvat();
     }
     settting_del_button();
 }
--- python-virtual-environments/static/js/table_interaction.js
+++ python-virtual-environments/static/js/table_interaction.js
@@ -1,10 +1,59 @@
 function totalprice_cal_vat(price,amount,vat)
 {
-    var total = 0.0 + (+(price) * +(amount));
-    var cVat = 1.0 + +(vat);
-    var total_vat = (total*cVat).toFixed(2);
+    var total_vat = 0.0;
+    var trans_price = price.replace(/,/g,'');
+    console.log("amount : " + amount);
+    console.log("price : " + parseInt(trans_price));
+    console.log("vat : " + vat);
+    var total = (parseInt(trans_price) * parseInt(amount));
+    console.log("total : " + total);
+    var cVat = 1.0 + (parseFloat(vat));
+    var price_vat = (total * vat);
+    total_vat = (total * cVat);
 
+    // return {
+    //     "t":total,
+    //     "v":(total * vat),
+    //     "n":total_vat
+    //    };
+    // console.log("T : " + total);
+    // console.log("V : " + price_vat);
+    // console.log("N : " + total_vat);
+    // return [total,price_vat,total_vat];
     return total_vat;
+}
+
+function updating_net_vat()
+{
+    var post_net = 0.0;
+    var post_vat = 0.0;
+
+    var table_r = document.getElementById("tab_logic");
+    // console.log(table_r.rows.length);
+    //console.log(table_r.rows[1].cells);
+    for (var i = 1; i < table_r.rows.length; ++i)
+    {
+        var f_pric = 'price'+(i-1);
+        var fd_price = document.getElementsByName(f_pric)[0].value;
+
+        var f_amoun = 'amount'+(i-1);
+        var fd_amoun = document.getElementsByName(f_amoun)[0].value;
+
+        cal_net = (parseFloat(fd_price.replace(/,/g,''))) * (parseFloat(fd_amoun.replace(/,/g,'')));
+        post_net = (parseFloat(post_net)) + (parseFloat(cal_net));
+
+        var f_vat = 'vat'+(i-1);
+        var fd_vat = document.getElementsByName(f_vat)[0].value;
+
+        cal_vat = cal_net * (parseFloat(fd_vat.replace(/,/g,'')));
+        post_vat = (parseFloat(post_vat)) + (+parseFloat(cal_vat));
+    }
+
+    document.getElementsByName("pricetotal0")[0].value = (post_net).toLocaleString('en-US', {minimumFractionDigits: 2});
+
+    document.getElementsByName("vattotal0")[0].value = (post_vat).toLocaleString('en-US', {minimumFractionDigits: 2});
+
+    document.getElementsByName("nettotal0")[0].value = (post_net + post_vat).toLocaleString('en-US', {minimumFractionDigits: 2});
 }
 
 function add_product_option(db_product, option_product)
@@ -23,24 +72,44 @@ function add_product_option(db_product, option_product)
     }
 }
 
+function updating_price_textfield(a,p,v,text_field)
+{
+    // console.log("UPDATE PRICE TX FIELD");
+    // var return_result = totalprice_cal_vat(a,p,v);
+    // console.log("T : " + return_result[0]);
+    // console.log("V : " + return_result[1]);
+    // console.log("N : " + return_result[2]);
+    // text_field.value = return_result[2];
+    // updating_net_vat(return_result[0],return_result[1]);
+    var cal_price = 0.0;
+    cal_price = totalprice_cal_vat(p,a,v);
+    console.log("CAL PRICE : " + cal_price);
+    text_field.value = (cal_price).toLocaleString('en-US', {minimumFractionDigits: 2});
+    updating_net_vat();
+}
+
 function set_db_product(db_product, sl_product,td_desc,tx_price,tx_totalprice,input_amount,sl_vat)
 {
     td_desc.value = db_product[0][1];
-    td_desc.disabled = true;
-    tx_price.value = db_product[0][2];
-    tx_price.disabled = true;
-    tx_totalprice.disabled = true;
+    // td_desc.disabled = true;
+    tx_price.value = (parseFloat(db_product[0][2])).toLocaleString('en-US', {minimumFractionDigits: 2});
+    // tx_price.disabled = true;
+    // tx_totalprice.disabled = true;
 
     sl_product.onchange = function()
     {
-        console.log("ON CHANGE!");
+        // console.log("PRODUCT ON CHANGE");
+        //console.log("ON CHANGE!");
         var sl_options = sl_product.selectedIndex;
-        console.log(sl_options);
+        //console.log(sl_options);
         td_desc.value = db_product[sl_options][1];
-        tx_price.value = db_product[sl_options][2];
+        tx_price.value = (parseFloat(db_product[sl_options][2])).toLocaleString('en-US', {minimumFractionDigits: 2});
         if(!!(input_amount) && !!(tx_price))
         {
-            tx_totalprice.value = totalprice_cal_vat(input_amount.value,tx_price.value,sl_vat.value);
+            updating_price_textfield(input_amount.value,tx_price.value,sl_vat.value,tx_totalprice);
+            // return_result = totalprice_cal_vat(input_amount.value,tx_price.value,sl_vat.value);
+            // tx_totalprice.value = return_result[2];
+            // updating_net_vat(return_result[0],return_result[1]);
         }
         else
         {
@@ -50,11 +119,25 @@ function set_db_product(db_product, sl_product,td_desc,tx_price,tx_totalprice,in
 
     input_amount.onchange = function()
     {
-        tx_totalprice.value = totalprice_cal_vat(input_amount.value,tx_price.value,sl_vat.value);
+        // console.log("AMOUNT ON CHANGE");
+        // console.log("T : " + input_amount.value);
+        // console.log("V : " + tx_price.value);
+        // console.log("N : " + sl_vat.value);
+        updating_price_textfield(input_amount.value,tx_price.value,sl_vat.value,tx_totalprice);
+        // return_result = totalprice_cal_vat(input_amount.value,tx_price.value,sl_vat.value);
+        // tx_totalprice.value = return_result[2];
+        // updating_net_vat(return_result[0],return_result[1]);
     }
 
     sl_vat.onchange = function()
     {
-        tx_totalprice.value = totalprice_cal_vat(input_amount.value,tx_price.value,sl_vat.value);
+        // console.log("VAT ON CHANGE");
+        // console.log("T : " + input_amount.value);
+        // console.log("V : " + tx_price.value);
+        // console.log("N : " + sl_vat.value);
+        updating_price_textfield(input_amount.value,tx_price.value,sl_vat.value,tx_totalprice);
+        // return_result = totalprice_cal_vat(input_amount.value,tx_price.value,sl_vat.value);
+        // tx_totalprice.value = return_result[2];
+        // updating_net_vat(return_result[0],return_result[1]);
     }
 }
