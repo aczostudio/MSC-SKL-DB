371de78083cccf795588910ed868405db1685ccc
Pakkanat Mee-Im <55268141+aczostudio@users.noreply.github.com>
2020-02-16 00:42:22 +0700

    Update heidi_sql_updateDB.sql

--- heidi_sql_updateDB/heidi_sql_updateDB.sql
+++ heidi_sql_updateDB/heidi_sql_updateDB.sql
@@ -12,21 +12,18 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping data for table msc-skl_db.clerk: ~0 rows (approximately)
-DELETE FROM `clerk`;
 /*!40000 ALTER TABLE `clerk` DISABLE KEYS */;
 /*!40000 ALTER TABLE `clerk` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.company: ~0 rows (approximately)
-DELETE FROM `company`;
 /*!40000 ALTER TABLE `company` DISABLE KEYS */;
-INSERT INTO `company` (`CompanyName`, `CompanyOwner`, `CompanyAddress`, `CompanyContactInfo`, `CompanyTaxID`) VALUES
+REPLACE INTO `company` (`CompanyName`, `CompanyOwner`, `CompanyAddress`, `CompanyContactInfo`, `CompanyTaxID`) VALUES
 	('Marine Survitec Songkhla Co.,Ltd.', 'Wanna Tanratanawong', '182/2 Sating-Mo Singhanakorn Songkhla 90280 Thailand', 'Tel.080-456-8001 Email:msc.songkhla@gmail.com', '000000000000');
 /*!40000 ALTER TABLE `company` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.customer: ~4 rows (approximately)
-DELETE FROM `customer`;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` (`CustomerName`, `CustomerAddress`, `CustomerContactInfo`, `CustomerTaxID`) VALUES
+REPLACE INTO `customer` (`CustomerName`, `CustomerAddress`, `CustomerContactInfo`, `CustomerTaxID`) VALUES
 	('Clearvac Engineering Asia Co.,Ltd. (Head office)', '20/78 หมู่2 ต.เกาะแก้ว อ.เมือง จังหวัดภูเก็ต 83000', '093-580-2588', '0835560008357'),
 	('S.J GLOBAL TRADE.,LTD PART.', '87 Chotivittayakul4 Road, Hatyai Songkhla 90110', '074-559-426 , 086-481-3478', NULL),
 	('บริษัท ซิลเวอร์ไลนิ่ง ภูเก็ต จำกัด(Head office)', '23/92 หมู่2 ต.เกาะแก้ว อ.เมือง จังหวัดภูเก็ต 83000', '000-0000', NULL),
@@ -34,9 +31,8 @@ INSERT INTO `customer` (`CustomerName`, `CustomerAddress`, `CustomerContactInfo`
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.customerproductorder: ~6 rows (approximately)
-DELETE FROM `customerproductorder`;
 /*!40000 ALTER TABLE `customerproductorder` DISABLE KEYS */;
-INSERT INTO `customerproductorder` (`CustomerProductOrderID`, `CustomerProductOrderDate`, `ProductName`, `CustomerProductOrderQuantity`, `CustomerProductOrderVat`, `ClerkID`, `CustomerName`) VALUES
+REPLACE INTO `customerproductorder` (`CustomerProductOrderID`, `CustomerProductOrderDate`, `ProductName`, `CustomerProductOrderQuantity`, `CustomerProductOrderVat`, `ClerkID`, `CustomerName`) VALUES
 	('quo20010002', '2020-01-28', 'BA-CYLN-HWAYAN', 1, 0.07, 'clerk_dummy', 'msit'),
 	('quo20010002', '2020-01-28', 'FE-AF-20L', 3, 0, 'clerk_dummy', 'msit'),
 	('quo20010003', '2020-01-28', 'BA-CYLN-HWAYAN', 1, 0.07, 'clerk_dummy', 'msit'),
@@ -46,14 +42,12 @@ INSERT INTO `customerproductorder` (`CustomerProductOrderID`, `CustomerProductOr
 /*!40000 ALTER TABLE `customerproductorder` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.employee: ~0 rows (approximately)
-DELETE FROM `employee`;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.product: ~5 rows (approximately)
-DELETE FROM `product`;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` (`ProductName`, `ProductDescription`, `ProductPrice`, `ProductUnit`, `ProductAmount`) VALUES
+REPLACE INTO `product` (`ProductName`, `ProductDescription`, `ProductPrice`, `ProductUnit`, `ProductAmount`) VALUES
 	('BA-CYLN-HWAYAN', 'BA Cylinder brand Hwayan', 10000, 'Cyln', 4),
 	('BA-CYLN-RONGUI', 'Ba Cylinder brand Rongui', 10000, 'Cyln', 15),
 	('FE-AF-20L', 'Foam AFFF 3% Size:20Ltrs.', 5500, 'Pcs', 5),
@@ -62,25 +56,21 @@ INSERT INTO `product` (`ProductName`, `ProductDescription`, `ProductPrice`, `Pro
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.quotation: ~2 rows (approximately)
-DELETE FROM `quotation`;
 /*!40000 ALTER TABLE `quotation` DISABLE KEYS */;
-INSERT INTO `quotation` (`QuotationID`, `QuotationDate`, `QuotationTotalPrice`, `QuotationVatPrice`, `QuotationNetTotal`, `CustomerProductOrderID`) VALUES
+REPLACE INTO `quotation` (`QuotationID`, `QuotationDate`, `QuotationTotalPrice`, `QuotationVatPrice`, `QuotationNetTotal`, `CustomerProductOrderID`) VALUES
 	('quo20010003', '2020-01-27', 11111, 111, 11222, 'quo20010003'),
 	('quo20010004', '2020-01-28', 26500, 700, 27200, 'quo20010004');
 /*!40000 ALTER TABLE `quotation` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.tools: ~0 rows (approximately)
-DELETE FROM `tools`;
 /*!40000 ALTER TABLE `tools` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tools` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.toolsrequisition: ~0 rows (approximately)
-DELETE FROM `toolsrequisition`;
 /*!40000 ALTER TABLE `toolsrequisition` DISABLE KEYS */;
 /*!40000 ALTER TABLE `toolsrequisition` ENABLE KEYS */;
 
 -- Dumping data for table msc-skl_db.vesselorrig: ~0 rows (approximately)
-DELETE FROM `vesselorrig`;
 /*!40000 ALTER TABLE `vesselorrig` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vesselorrig` ENABLE KEYS */;
 
